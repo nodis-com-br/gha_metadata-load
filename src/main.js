13f1394c375fa0c6ed27a7b5972ddc1d69f7e17d
ptonini <pedro.tonini@nodis.com.br>
2021-08-09 20:11:13 -0300

    refactoring

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const artifact = require('@actions/artifact');
+const fs = require('fs');
+const process = require('process');
+
+const artifactClient = artifact.create();
+artifactClient.downloadArtifact('metadata').then(result => {
+
+    const sourceJson = JSON.parse(fs.readFileSync('./metadata.json'));
+    const destinyStream = fs.createWriteStream(process.env.GITHUB_ENV, {flags:'a'});
+
+    for (const k in sourceJson) if (sourceJson.hasOwnProperty(k)) {
+        const env = 'NODIS_' + k;
+        core.info(env);
+        destinyStream.write(env + '=' + sourceJson[k] + '\n');
+    }
+
+}).catch(err => core.setFailed(err));
